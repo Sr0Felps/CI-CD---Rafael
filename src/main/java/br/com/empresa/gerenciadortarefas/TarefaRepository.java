80372e45a4f3d409dbd3496bb6113f74d5c3fce4
Filipe Costa Junqueira <filipecj0@gmail.com>
2025-09-19 17:33:47 -0300

    feat(init): Versão inicial da API de Gerenciamento de Tarefas

---
+++ src/main/java/br/com/empresa/gerenciadortarefas/TarefaRepository.java
@@ -0,0 +1,6 @@
+package br.com.empresa.gerenciadortarefas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
